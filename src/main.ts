76c9396ac7952df3ccc09c46c608b25784afa052
Nick Ezekiel <nickezekias@gmail.com>
2024-06-13 20:09:01 +0100

    installed and configured primevue

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useI18n } from 'vue-i18n'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
 import i18n from './locales/i18n'
+
+import Button from 'primevue/button'
 
 const app = createApp(App, {
   setup() {
@@ -18,5 +22,18 @@ const app = createApp(App, {
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(PrimeVue, {
+  ripple: true,
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false
+    }
+  }
+})
+
+app.component('PrimeButton', Button)
 
 app.mount('#app')
