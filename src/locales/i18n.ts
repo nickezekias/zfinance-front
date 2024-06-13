2ead6bf13b47c7c687e236e67f4579c475127c8d
Nick Ezekiel <nickezekias@gmail.com>
2024-06-13 19:22:25 +0100

    configured i18n locale and fallback locale to use env vars

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -4,8 +4,8 @@ import fr from '@/locales/fr.json'
 
 const i18n = createI18n({
   legacy: false, // you must set `false`, to use Composition API
-  locale: 'en', // set locale
-  fallbackLocale: 'en', // set fallback locale
+  locale: import.meta.env.VITE_I18N_LOCALE, // set locale
+  fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE, // set fallback locale
   messages: {
     fr,
     en
