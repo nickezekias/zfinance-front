5c44081e7087dac36376afcdfa560e503b6e9e2e
Nick Ezekiel <nickezekiel@ignislab.com>
2024-06-29 00:24:16 +0100

    added user model

---
+++ src/app/domain/user.model.ts
@@ -0,0 +1,46 @@
+export default class User {
+  id: number
+  avatar: string
+  createdAt: string
+  email: string
+  emailVerfiedAt: string | null
+  firstName: string
+  IDDocument: string | null
+  IDDocumentVerifiedAt: string | null
+  isActive: boolean
+  lastName: string
+  phone: string
+  phoneVerifiedAt: string | null
+  updatedAt: string
+
+  public constructor(
+    id: number,
+    avatar: string,
+    createdAt: string,
+    email: string,
+    emailVerifiedAt: string,
+    firstName: string,
+    IDDocument: string | null,
+    IDDocumentVerifiedAt: string | null,
+    isActive: boolean,
+    lastName: string,
+    phone: string,
+    phoneVerifiedAt: string,
+    updatedAt: string
+
+  ) {
+    this.id = id
+    this.avatar = avatar
+    this.createdAt = createdAt
+    this.email = email
+    this.emailVerfiedAt = emailVerifiedAt
+    this.firstName = firstName
+    this.IDDocument = IDDocument
+    this.IDDocumentVerifiedAt = IDDocumentVerifiedAt
+    this.isActive = isActive
+    this.lastName = lastName
+    this.phone = phone
+    this.phoneVerifiedAt = phoneVerifiedAt
+    this.updatedAt = updatedAt
+  }
+}
