d194de71a05ac154b1dab345dadd3b63af6b0b60
Nick Ezekiel <nickezekiel@ignislab.com>
2024-07-09 19:48:05 +0100

    created app content component

--- src/app/features/dashboard/presentation/IndexView.vue
+++ src/app/features/dashboard/presentation/IndexView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import type { Ref } from 'vue'
+
+import AppContent from '@/layouts/AppContent.vue'
+
+const { t } = useI18n()
+
+const breadcrumbItems: Ref<Array<Record<string, string>>> = ref([{ label: t('labels.dashboard') }])
+</script>
 
 <template>
-  <h1>Dashboard</h1>
+  <AppContent :breadcrumbItems="breadcrumbItems">
+    <template #header>
+      <span class="font-light text-xl md:text-2xl text-gray-600">{{ $t('labels.dashboard') }}</span>
+    </template>
+  </AppContent>
 </template>
---
+++ src/layouts/AppContent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import PrimeBreadcrumb from 'primevue/breadcrumb'
+
+const props = defineProps<{
+  breadcrumbItems: Array<Record<string, string>>
+}>()
+
+const home = ref({
+  icon: 'pi pi-home'
+})
+</script>
+
+<template>
+  <div class="w-full">
+    <slot name="header"></slot>
+    <PrimeBreadcrumb :home="home" :model="props.breadcrumbItems" class="p-0 m-0 text-sm" />
+    <slot></slot>
+  </div>
+</template>
