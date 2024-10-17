5c551a6919ef5e23922e40a77486dcbeba7f9056
Nick Ezekiel <nickezekias@gmail.com>
2024-10-17 20:46:53 +0100

    made admin middleware use adminAuthStore pinia store

--- src/router/middleware/admin.middleware.ts
+++ src/router/middleware/admin.middleware.ts
@@ -1,10 +1,25 @@
-import { useAuthStore } from "@/stores/auth.store";
+import { useAdminAuthStore } from "@/stores/admin.auth.store";
 
-export default function admin({ next }: any) {
-  const authStore = useAuthStore()
-  if (authStore.isAdmin()) {
-    next()
+export default async function admin({ to, next }: any) {
+  const authStore = useAdminAuthStore()
+  const loginQuery = { path: '/admin/login', query: { redirect: to.fullPath } }
+  if (authStore.isGuest()) {
+    authStore.clearAuthenticatedUser()
+    next(loginQuery)
   } else {
-    next({ name: 'notFound' })
+    if (authStore.user) {
+      next()
+    } else {
+      try {
+        await authStore.getAuthenticatedUser()
+      } catch(e) {
+        console.error(e)
+      }
+      if (authStore.user) {
+        next()
+      } else {
+        next(loginQuery)
+      }
+    }
   }
 }
